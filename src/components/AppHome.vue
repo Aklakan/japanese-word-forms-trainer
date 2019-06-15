a92afacfb102369071a21e8f0533b2b4ebaefa5c
Claus Stadler <cstadler@informatik.uni-leipzig.de>
2019-06-15 10:27:04 +0200

    Some fixes

--- src/components/AppHome.vue
+++ src/components/AppHome.vue
@@ -1,55 +1,54 @@
 <template>
   <div class="container bg">
     <div class="row" style="justify-content:center;">
-      <h1>Howdy adventurer,</h1>
+      <h1>Howdy Otaku,</h1>
     </div>
     <div class="row">
       <div class="col-sm-6 offset-sm-3 text-center">
-        <h2>What is the <span class="badge badge-pill badge-info">{{task.data.tgtWordFormLbl}}</span> form of</h2>
+        <h2>What is the <span class="badge badge-pill badge-info">{{taskData.tgtWordFormLbl}}</span> form of</h2>
       </div>
     </div>
     <div class="row">
       <div class="col-sm-6 offset-sm-3 text-center">
-        <h2><span class="badge badge-pill badge-info">{{task.data.srcWordKanjiLbl}}</span></h2>
+        <h2><span class="badge badge-pill badge-info">{{taskData.srcWordKanjiLbl}}</span></h2>
       </div>
     </div>
     <div class="row" v-show="showSpoiler">
       <div class="col-sm-6 offset-sm-3 text-center">
-        <span class="text-muted font-italic">{{task.data.srcWordTrnsLbl}} - {{task.data.srcWordRentaikeiLbl}} ({{task.data.srcWordHiraganaLbl}})</span>
+        <span class="text-muted font-italic">{{taskData.srcWordTrnsLbl}} - {{taskData.srcWordRentaikeiLbl}} - {{taskData.srcWordHiraganaLbl}}</span>
       </div>
     </div>
 
-    <div class="form-text"></div>
-
-
-    <div class="row">
+    <div class="row"　v-show="!taskDone">
       <div class="col-sm-6 offset-sm-3 text-center">
+<div class="btn-group" role="group">
         <button type="button" class="btn btn-outline-warning btn-sm" v-on:click='showSpoiler = !showSpoiler'>{{showSpoiler ? 'Hide' : 'Show'}} spoiler</button>
+</div>
       </div>
     </div>
 
     <div class="row">
       <div class="col-sm-6 offset-sm-3 text-center">
-<form>
-        <fieldset :disabled="task.done">
+
+        <fieldset :disabled="taskDone">
         <div class="form-group">
 <!--          <div class="form-group"> -->
-            <input class="form-control form-control-lg" v-bind:class="{ 'is-invalid': task.fail, 'is-valid': task.success }" type="text" v-model="answer" @input="validate">
+            <input class="form-control form-control-lg" v-bind:class="{ 'is-invalid': taskFail, 'is-valid': taskSuccess }" type="text" v-model="answer" @input="validate">
           </div>
 <!--        </div> -->
 </fieldset>
-</form>
+
 </div>
     </div>
 
     <div class="form-text"></div>
 
     <div class="row">
-      <div class="col-sm-2 offset-sm-3 text-center" v-show="!task.done">
+      <div class="col-sm-2 offset-sm-3 text-center" v-show="!taskDone">
         <button type="button" class="btn btn-danger" v-on:click="giveUp()">Show solution</button>
       </div>
 
-      <div class="col-sm-2 offset-sm-3 text-center" v-show="task.done">
+      <div class="col-sm-2 offset-sm-3 text-center" v-show="taskDone">
         <button type="button" class="btn btn-success" v-on:click="newTask()">Next</button>
       </div>
 
@@ -64,41 +63,50 @@ export default {
   name: 'AppHome',
   data() {
     return {
-      task: {data: {}},
+      taskData:{},
+      taskSuccess: false,
+      taskDone: false,
+      taskFail: false,
       showSpoiler: false,
       answer: ''
     };
+  },
+  watch: {
+    'taskDone'(v) {
+      this.showSpoiler = v;
+    }
   },
   created() {
     this.newTask();
   },
   methods: {
+
     newTask() {
-      while(this.task.done) {
+      do {
         this.newTaskCore();
-      }
+      } while(this.taskDone);
     },
 
     // Sometimes it happens that a task is immediately solved, in that case just generate a new one
     newTaskCore() {
-      this.task.data = pickTask();
-      this.task.done = false;
-      this.task.fail = false;
-      this.task.success = false;
-      this.answer = this.task.data.srcWordKanjiLbl;
+      this.taskData = pickTask();
+      this.taskDone = false;
+      this.taskFail = false;
+      this.taskSuccess = false;
+      this.answer = this.taskData.srcWordKanjiLbl;
       this.validate();
     },
 
     giveUp() {
-      this.answer = this.task.data.tgtWordLbl;
-      this.task.done = true;
-      this.task.fail = true;
+      this.answer = this.taskData.tgtWordLbl;
+      this.taskDone = true;
+      this.taskFail = true;
     },
 
     validate() {
-     if(this.answer == this.task.data.tgtWordLbl) {// || this.answer == 'test') {
-       this.task.done = true;
-       this.task.success = true;
+     if(this.answer == this.taskData.tgtWordLbl) {// || this.answer == 'test') {
+       this.taskDone = true;
+       this.taskSuccess = true;
      }
     }
   }
